e682f9021492f1516c85f9301c7b6a1f5b871e53
Sergio Martins <sergio.cmartins@gmail.com>
2020-09-08 15:42:53 -0300

    Creating First Model and Scaffolding the view and controller

--- SalesWeb/Controllers/HomeController.cs
+++ SalesWeb/Controllers/HomeController.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using SalesWeb.Models;
+using SalesWeb.Models.ViewModels;
 
 namespace SalesWeb.Controllers
 {
